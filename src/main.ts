abe0024c5f63f57cc1162b2ec45b0be1a125a953
Stharley Maxwell <stharleymax@gmail.com>
2021-12-23 17:32:54 -0300

    add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/Http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 }
 bootstrap();
