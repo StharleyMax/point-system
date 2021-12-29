0d83734964c43cb191d9b2a22c5ffb58dc8010fb
Stharley Maxwell <stharleymax@gmail.com>
2021-12-29 10:44:21 -0300

    merge

--- src/api/pointRecord/pointRecord.module.ts
+++ src/api/pointRecord/pointRecord.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
 import { PointRepository } from 'src/database/repository/point.repository';
-=======
-import { PointRepository } from 'database/repository/point.repository';
->>>>>>> cacfbc6f848935fdce6ce2044026977c1f1b70f8
 import { PointRecordController } from './pointRecord.controller';
 import { PointRecordService } from './pointRecord.service';
 
