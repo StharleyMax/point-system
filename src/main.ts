99e13596bf4eb1aea82809c93fa44ed0985dd9a4
DESKTOP-N9VHQK4\Marcio <marciocarolino@gmail.com>
2021-12-30 15:41:27 -0300

    Não foi adicionado o REDIS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { NestMicroserviceOptions } from '@nestjs/common/interfaces/microservices/nest-microservice-options.interface';
 import { NestFactory } from '@nestjs/core';
-
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/Http-exception.filter';
 
+
+//Create a Logger instance
+const logger = new Logger('Main');
+/*
+const microserviceOptions = {
+  transport: Transport.REDIS,
+  options: {
+    url: 'redis://localhost:6379',
+  },
+};
+*/
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.listen(3000, () => {
+    logger.log('Sistema rodando!!');
+  });
 
   app.useGlobalFilters(new AllExceptionsFilter());
 
@@ -18,4 +33,5 @@ async function bootstrap() {
     }),
   );
 }
+
 bootstrap();
