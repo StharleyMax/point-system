0c92d298f4adcf5d2cd78abeea2d058afd05b66b
DESKTOP-N9VHQK4\Marcio <marciocarolino@gmail.com>
2021-12-30 16:17:32 -0300

    configurando DTO para swagger

--- src/api/pointRecord/dto/createPointRecord.dto.ts
+++ src/api/pointRecord/dto/createPointRecord.dto.ts
@@ -1,11 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreatePointRecordDTO {
 
+  @ApiProperty()
   user!: any;
+
+  @ApiProperty({ type: Date })
   startPoint!: Date;
+
+  @ApiProperty({ type: Date })
   startLunch!: Date;
+
+  @ApiProperty({ type: Date })
   returnLunch!: Date;
+
+  @ApiProperty({ type: Date })
   exitPoint!: Date;
+
+  @ApiProperty({ type: Date })
   createdAt: Date;
+
+  @ApiProperty({ type: Date })
   updatedAt: Date;
 }
 
--- src/api/pointRecord/dto/updatePointRecord.dto.ts
+++ src/api/pointRecord/dto/updatePointRecord.dto.ts
@@ -1,8 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UpdateCreatePointRecordDTO {
 
+  @ApiProperty()
   user: string;
+
+  @ApiProperty({ type: Date })
   startPoint: Date;
+
+  @ApiProperty({ type: Date })
   startLunch: Date;
+
+  @ApiProperty({ type: Date })
   returnLunch: Date;
+
+  @ApiProperty({ type: Date })
   exitPoint: Date;
 }
